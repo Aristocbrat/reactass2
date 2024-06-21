de691c8059576b89f25bcd744586a4f40f18b689
Aristocbrat <emmanuelforbez@gmail.com>
2024-06-21 16:20:02 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import MyButtons from './componenets/Weekdays';
+
+
+function App() {
+  return (
+    <div className='bg-dark'>
+      <MyButtons/>
+    </div>
+  )
+}
+
+export default App;
